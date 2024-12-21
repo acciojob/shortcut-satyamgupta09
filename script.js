55a7f44663a67cfb8dbdb10f0ad9b6b63cb6b203
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-21 21:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function shortcut(s1, s2) {
   // your code here
-}
+	let size1=s1.length;
+	let size2=s2.length;
+
+	if(size1>0 && size2>0){
+		let s=s1[0]+s2[0];
+		return s;
+	}
+	else if(size1==0 || size2==0){
+		return "";
+	}
+} 
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
+const s1 = prompt("Enter s1:"); 
 const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+alert(shortcut(s1, s2)); 
